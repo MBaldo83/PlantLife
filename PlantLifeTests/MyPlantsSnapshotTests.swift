bb338b81b0fde3584484ee76b0b79a056055da0a
Michael Baldock <michael.baldock@telegraph.co.uk>
2019-03-15 15:57:48 +0000

    Implemented 2 snapshot testing methods image based, and hierarchy based.

---
+++ PlantLifeTests/MyPlantsSnapshotTests.swift
@@ -0,0 +1,26 @@
+//
+//  MyPlantsSnapshotTests.swift
+//  PlantLifeTests
+//
+//  Created by Michael Baldock on 15/03/2019.
+//  Copyright © 2019 Michael Baldock. All rights reserved.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import PlantLife
+
+class MyPLantsSnapshotTests: XCTestCase {
+
+  func testAppCoordinatorSnapshotAsImage() {
+
+    let vc = AppCoordinator()
+    assertSnapshot(matching: vc, as: .image)
+  }
+
+  func testAppCoordinatorSnapshotAsHierarchy() {
+
+    let vc = AppCoordinator()
+    assertSnapshot(matching: vc, as: .hierarchy)
+  }
+}
--- Podfile
+++
@@ -1,8 +0,0 @@
-platform :ios, '12.1'
-use_frameworks!
-
-target 'PlantLifeTests' do
-
-  pod 'SnapshotTesting', '~> 1.2'
-
-end
